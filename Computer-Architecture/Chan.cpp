ff58796c3ff95b6782386ce342b026397fbfe7ea
Caphile <47132589+Caphile@users.noreply.github.com>
2022-05-26 07:33:06 +0900

    Add files via upload

---
+++ Computer-Architecture/Chan.cpp
@@ -0,0 +1,19 @@
+/*
+#include "common/ALU.h"
+#include "common/big-endian to little-endian.h"
+#include "common/bitwise, shift op.h"
+#include "common/disassemble.h"
+#include "common/disassembler.h"
+#include "common/instruction fetch and decode.h"
+#include "common/memory modeling.h"
+*/
+
+#include "common/test.h"
+
+#include <stdio.h>
+
+int main() {
+	printf("%d", add(1, 2));
+
+	return 0;
+}
